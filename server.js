02b7cecac2e50375e973672fb4c1ef1f68488116
Marcell Lévai <levaimarcell65@gmail.com>
2024-01-16 16:41:43 +0100

    catching implemented

--- server.js
+++ server.js
@@ -7,36 +7,50 @@ const server = http.createServer((req, res) => {
     const path = parsedUrl.pathname;
     const { userId } = parsedUrl.query;
 
-    if(req.method === 'GET'){
-        if(path === '/users'){
+    
+    if (req.method === 'GET') {
+        if (path === '/users') {
             getUsers(res);
-        }else if(path === '/user/hobbies'){
+        } else if (path === '/user/hobbies') {
+            res.setHeader('Cache-Control', 'public, max-age=3600');
             getUserHobbies(res, userId);
-        }else if('/user'){
+        } else if (path === '/user') {
             getUser(res, userId);
+        } else {
+            handleRouteNotFound(res);
         }
-    }else if(req.method === 'POST'){
-        if(path === '/user'){
+    } else if (req.method === 'POST') {
+        if (path === '/user') {
             createUser(req, res);
-        }else if (path === '/user/hobbies') {
+        } else if (path === '/user/hobbies') {
             addHobby(req, res, userId);
-        }    
-    }else if(req.method === 'PUT'){
-        if(path === '/user'){
-            updateUser(req, res, userId);
+        } else {
+            handleRouteNotFound(res);
         }
-    }else if (req.method === 'DELETE') {
-        if(path === '/user'){
+    } else if (req.method === 'PUT') {
+        if (path === '/user') {
+            updateUser(req, res, userId);
+        } else {
+            handleRouteNotFound(res);
+        }
+    } else if (req.method === 'DELETE') {
+        if (path === '/user') {
             deleteUser(req, res, userId);
-        }else if(path === '/user/hobbies'){
+        } else if (path === '/user/hobbies') {
             const { hobbyName } = parsedUrl.query;
             deleteHobby(req, res, userId, hobbyName);
+        } else {
+            handleRouteNotFound(res);
         }
-    }else{
-        res.writeHead(404, { 'Content-Type': 'application/json'});
-        res.end(JSON.stringify({ message: 'Route Not Found' }));
+    } else {
+        handleRouteNotFound(res);
     }
 });
+
+function handleRouteNotFound(res) {
+    res.writeHead(404, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Route Not Found' }));
+}
 
 const PORT = process.env.PORT || 5000;
 
