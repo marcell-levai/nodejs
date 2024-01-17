51a3874897463a9d5283de1793652db4f52dc80e
Marcell Lévai <levaimarcell65@gmail.com>
2024-01-17 11:41:02 +0100

    json format fixed

--- task3/task3.js
+++ task3/task3.js
@@ -7,7 +7,16 @@ const txtFilePath = './csvdirectory/data.txt';
 
 pipeline(
   fs.createReadStream(csvFilePath),
-  csv(),
+  csv({
+    delimiter: '\t',
+    trim: true,
+    output: 'json',
+    headers: ['book', 'author', 'amount', 'price'],
+    colParser: {
+      amount: 'omit',
+    },
+    checkType: false,
+  }),
   fs.createWriteStream(txtFilePath),
   (error) => {
     if (error) {
